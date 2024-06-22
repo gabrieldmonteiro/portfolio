a1696c31c898847a3e8e249d628757f7d601fbad
Gabriel <gabrieldmonteiro@live.com>
2024-06-22 03:28:32 -0300

    ✨feat: play and github buttons

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom"
 import '../style/projectCardStyle.css'
 import githubIcon from "../img/github.png"
 import playIcon from "../img/play.png"
@@ -6,21 +7,29 @@ import playIcon from "../img/play.png"
 const ProjectCard = (props) => {
   return (
     <div className="main-project-div">
-    <div className="project-card">
-      <div>
-        <p><b>{props.name}</b></p>      
+      <div className="project-card">
+        <div>
+          <p><b>{props.name}</b></p>
+        </div>
+        <div>
+          <img alt="" className="card-project-image" src={props.image}></img>
+        </div>
+        <div className="project-description">
+          {props.description}
+        </div>
+        <div className="card-link-div">
+          {props.url && (
+            <Link to={props.url} className="card-url" target="_blank">
+              <img className="project-icons" src={playIcon} alt="app" />
+            </Link>
+          )}
+          {props.url_gh && (
+            <Link to={props.url_gh} className="card-url" target="_blank" >
+              <img className="project-icons" src={githubIcon} alt="github" />
+            </Link>
+          )}
+        </div>
       </div>
-      <div>
-        <img alt="" className="card-project-image" src={props.image}></img>
-      </div>
-      <div className="project-description">        
-        {props.description}
-      </div>      
-      <div className="card-link-div">        
-        {props.url && (<a href={props.url} className="card-url"><img className="project-icons" src={playIcon} alt="app" target="_blank"></img></a>)}
-        {props.url_gh && (<a href={props.url_gh} className="card-url"><img className="project-icons" src={githubIcon} alt="github" target="_blank"></img></a>)}
-      </div>
-    </div>
     </div>
   );
 };
